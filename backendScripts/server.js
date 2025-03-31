adaa3143b368fd01f2eb8fec0184d54445babcb9
Jenny O'Halloran <jenny.ohalloran@cranfield.ac.uk>
2025-03-31 07:59:08 +0100

    Server Endpoints Updated - Commented and redundants removed

--- backendScripts/server.js
+++ backendScripts/server.js
@@ -1,12 +1,16 @@
 
+/*** 
+ * // Date: 28/03/2025
+ * // Jennifer O'Halloran
+ * // IBIX2 Group Project 2025 
+***/
 
-// Date: 28/03/2025
-// Jennifer O'Halloran
-// IBIX2 Group Project 2025 
+/***
+// Function  - Express API SERVER
+// Endpoints for the Front-end Angular Post Requests 
+***/
 
-
-// Function  - API SERVER
-
+// Importing all required functions
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
@@ -20,60 +24,34 @@ const app = express();
 // Middleware
 app.use(cors()); // To allow cross-origin requests
 app.use(bodyParser.json()); // To parse JSON request bodies
+app.use(express.text()); // To parse selected ec code from user as string 
 
 
- 
-// Calles Process Input from Merge.js 
-app.get('/api/getModel/:code', (req, res) => {
+
+  //------------ Retrieving Mapping Data -------------
+  // 
+app.post('/api/getMap', (req, res) => {
     console.log('Getting Diagram Model');
-    // Wait for response - async function
-    processInput(req.params.code).then(elements => res.json(elements.map_elements));
+    var pathway = req.body;
+    console.log(pathway);
+    processInput(pathway).then(elements => res.json(elements));
 
   });
 
 
+  //------------ Retrieving Enzyme Pathways -------------
+  // Takes list of EC codes from Front-end 
+  //  - Gets Enzyme Map Codes (e.g. ec00030) - getEnzymePathways()
+  //  - Gets Corresponding Name - getPathwayNames()
+  //  - Returns Array of Pathway Objects with Name and Pathway Code
 
-// Calles Process Input from Merge.js 
-app.post('/api/getModel', (req, res) => {
-    console.log('Getting Diagram Model');
-    // Wait for response - async function
-    
-    processInput(req.body).then(elements => res.json(elements));
-
-  });
-
-
-  // POST REQUEST FROM SERVER 
-  app.post('/api/getEnzymePathways', (req, res) => {
+app.post('/api/getPathways', (req, res) => {
     // Handling a post request of Enzyme codes?? - list of enzyme code expected
     console.log('Getting Enzyme Pathways');
     console.log(req.body);
     var enzymes = req.body;
     getEnzymePathways(enzymes).then(pathways => getPathwayNames(pathways))
         .then((result)=>{
-            //console.log(result)
-            //console.log(result.paths[0]);
-            res.json(result.paths);
-        });
-
-  });
-
-// GET REQUEST  - REDUNDANT 
-  app.get('/api/getEnzymePathways/enzymes2', (req, res) => {
-    // Handling a post request of Enzyme codes?? - list of enzyme code expected
-    const enzymes = [
-        'ec:1.1.1.360',
-        'ec:1.1.1.359',
-        'ec:4.3.1.29',
-        'ec:5.3.1.27',
-        'ec:5.3.1.29',
-        'ec:2.7.1.212',
-        'ec:1.2.1.90'];
-    
-    getEnzymePathways(enzymes).then(pathways => getPathwayNames(pathways))
-        .then((result)=>{
-            //console.log(result)
-            console.log(result.paths[0]);
             res.json(result.paths);
         });
   });
