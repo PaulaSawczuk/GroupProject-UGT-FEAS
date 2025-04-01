a690acbf7b44b5b1075db33319670b44b4edbf15
Jenny O'Halloran <jenny.ohalloran@cranfield.ac.uk>
2025-04-01 13:51:05 +0100

    Minor Changes

--- kegg_pathway/src/app/services/kegg_enzymepathwaysPost.serice.ts
+++ kegg_pathway/src/app/services/kegg_enzymepathwaysPost.serice.ts
@@ -25,7 +25,7 @@ import { Observable } from 'rxjs';
 
 export class enzymeApiServicePost {
 
-  private apiUrl = 'http://localhost:3000/api/';
+  private apiUrl = 'http://localhost:4000/api/';
 
   constructor(private http: HttpClient) {}
 
